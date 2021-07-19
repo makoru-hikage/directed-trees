9c96eaefc431e8957906eb0aed487d1afc93e51d
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-07-20 01:32:49 +0800

    Overhaul: fix and improve Edge module

--- Edge.mjs
+++ Edge.mjs
@@ -1,17 +1,32 @@
-const hasVertex  = (self) => (
-    (vertex) => {
-        return self.firstVertex.id === vertex.id ||
-            self.secondVertex.id === vertex.id
-    }
-)
+const hasVertex  = (self) => ({
+  /**
+   * Check whether an edge is connecting
+   * a certain vertex
+   * 
+   * @param vertex 
+   * @returns 
+   */
+  containsVertex (vertex) {
+    return self.firstVertex.id === vertex.id 
+      || self.secondVertex.id === vertex.id
+  }
+})
 
+/**
+ * A part of a Graph that connects two Vertices
+ * 
+ * @param Vertex firstVertex 
+ * @param Vertex secondVertex 
+ * @returns Edge
+ */
 export default function Edge (firstVertex, secondVertex) {
-    let edge = {
-        firstVertex,
-        secondVertex
-    }
+  let edge = {
+    firstVertex,
+    secondVertex
+  }
 
-    return Object.assign(
-        edge
-    )
+  return Object.assign(
+    edge,
+    hasVertex(edge)
+  )
 }
