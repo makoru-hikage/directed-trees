a9245f735a9d060182c393f177a92ca68bd685f4
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-07-21 07:13:45 +0800

    Add comments

--- Forest.mjs
+++ Forest.mjs
@@ -1,3 +1,8 @@
+/**
+ * A Forest is a graph whose subgraphs
+ * are valid trees.
+ *
+ */
 const ForestTrait = (directedGraph) => ({
   /**
    * Find any vertex that isn't a head of any edge
@@ -17,36 +22,48 @@ const ForestTrait = (directedGraph) => ({
   },
 
   /**
-   * To nidify is to create a nest.
-   * Create a nest of vertices and
-   * makes it a property of a graph
+   * nidify (n.) - to create a nest.
+   * Create nests of vertices
    *
-   * @returns DirectedGraph
+   * @returns array of nested objects
    */
    toForest () {
     let vertices = directedGraph.vertices
 
+    /**
+     * A function made just to recurse well
+     */
     const nidify = vertex => {
+      // Define 'depth' property should
+      // it not exist.
       if (!('depth' in vertex)){
         vertex.depth = 0
       }
+
       vertex.children = directedGraph
+        // Find children
         .findVertexHeads(vertex.id)
+        // Sort them
         .sort((v1, v2) => v1.seqId - v2.seqId)
+        // Add their corresponding depth
         .map(child => {
           child.depth = vertex.depth + 1
           return child
         })
+
       return vertex
     }
 
+
     return vertices
-      .map(nidify)
+      // Find the 'roots' of the forest
       .filter( potentialRoot => 
         directedGraph
           .findRootVertices()
           .some(vertex => vertex.id === potentialRoot.id)
       )
+      // Nidify them
+      .map(nidify)
   }
 })
 
