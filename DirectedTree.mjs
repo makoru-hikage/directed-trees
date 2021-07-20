37a49af905f2e96d98784a0eac6bf1afd5f639fc
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-07-21 00:05:19 +0800

    Overhaul on DirectedTree module

--- DirectedTree.mjs
+++ DirectedTree.mjs
@@ -1,36 +1,62 @@
-import Edge from "./Edge.mjs"
+import Edge from './Edge.mjs'
 import {Graphness} from './Graph.mjs'
 import {Directedness} from './DirectedGraph.mjs'
 
-const TreeChecks = {
-  findRootVertex (directedGraph){
-    if (directedGraph.vertices.length <= 0) {
-      return null
-    }
 
-    return directedGraph.vertices.find((vertex) =>{
-      let vertexId = vertex.id
-      let numOfTails = directedGraph.findVertexTails(vertexId).length
-      return numOfTails <= 0
-    })
-  },
+const findRootVertex = (directedGraph) => {
+  if (directedGraph.vertices.length <= 0) {
+    return null
+  }
 
-  allHaveOneParent (directedGraph) {
-    if (directedGraph.vertices.length <= 0) {
+  return directedGraph.vertices.find((vertex) =>{
+    let vertexId = vertex.id
+    let numOfTails = directedGraph.findVertexTails(vertexId).length
+    return numOfTails <= 0
+  })
+}
+
+const allHaveOneParent = (directedGraph) => {
+  if (directedGraph.vertices.length <= 0) {
+    return false
+  }
+
+  for (let i = 0; i < directedGraph.vertices.length; i++) {
+    let vertexId = directedGraph.vertices[i].id
+    let numOfTails = directedGraph.findVertexTails(vertexId).length
+
+    if (numOfTails > 1) {
       return false
     }
-
-    for (let i = 0; i < directedGraph.vertices.length; i++) {
-      let vertexId = directedGraph.vertices[i].id
-      let numOfTails = directedGraph.findVertexTails(vertexId).length
-
-      if (numOfTails > 1) {
-        return false
-      }
-    }
-
-    return true
   }
+
+  return true
+}
+
+/**
+ * 
+ * @param DirectedGraph directedGraph 
+ * @param int vertexId - the starting vertex
+ * @param edgesPassed 
+ * @return array of Edges
+ */
+const findPathToRoot = (directedGraph, vertexId, edgesPassed = []) => {
+  let vertex = directedGraph.findVertex(vertexId)
+
+  if (vertex === null){
+    return edgesPassed
+  }
+
+  let edges = directedGraph.findVertexTails(vertexId)
+
+  if (edges.length !== 1) {
+    return edgesPassed
+  }
+
+  let tail = edges[0].firstVertex.id
+
+  edgesPassed.push(edges[0])
+  return findPathToRoot(directedGraph, tail, edgesPassed)
+
 }
 
 const AddsVertices = (self) => ({
@@ -63,8 +89,17 @@ const AddsVertices = (self) => ({
 })
 
 /**
- * Each vertex has only one parent if there are any
- * Only the rootVertex has no parent
+ * A graph is a tree when:
+ * 1. There is only one root vertex which has
+ * no 'parent', meaning, there are no edges in
+ * a graph where it is a head.
+ * 2. All vertices other than the root has only
+ * one parent. None of the vertices have more
+ * than one parent. Any vertex can have none or
+ * more children.
+ * 3. All paths from root to any children are
+ * unforked and acyclic. Meaning, no circular
+ * path or forked paths.
  *
  * @param Vertex rootVertex 
  * @returns 
@@ -88,5 +123,7 @@ function DirectedTree (rootVertex) {
 
 export {
   DirectedTree,
-  TreeChecks
+  findRootVertex,
+  allHaveOneParent,
+  findPathToRoot
 }
