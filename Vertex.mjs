ce47af542d2c5c1f6ea0de18e4175b1d401b7ef5
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-07-21 00:01:56 +0800

    Overhaul on Vertex module

--- Vertex.mjs
+++ Vertex.mjs
@@ -1,29 +1,30 @@
 
-const vertexChecks = (self) => ({
-    /**
-     * Should this return a positive number, 
-     * it means that `vertex2` precedes `vertex1`,
-     * should it be zero, it has the same precedence.
-     * If `vertex2` has negative precedence, it follows.
-     * Made for Array.prototype.sort()
-     *
-     * @param Vertex anotherVertex 
-     * @returns int
-     */
-    compareVertex (vertex1, vertex2 = self) {
-        return vertex1.seqId - vertex2.seqId
-    },
 
-    /**
-     * Find a certain vertex in an array of vertices
-     *
-     * @param vertices - haystack
-     * @returns Vertex - itself
-     */
-    findInVertices (vertices) {
-        vertices.find( vertex => vertex.id === self.id )
-    }
-})
+/**
+ * Should this return a positive number, 
+ * it means that `vertex2` precedes `vertex1`,
+ * should it be zero, it has the same precedence.
+ * If `vertex2` has negative precedence, it follows.
+ * Made for Array.prototype.sort()
+ *
+ * @param Vertex anotherVertex 
+ * @returns int
+ */
+const compareVertices = (vertex1, vertex2) => {
+    return vertex1.seqId - vertex2.seqId
+}
+
+/**
+ * Find a certain vertex in an array of vertices
+ *
+ * @param array - haystack
+ * @param int - needle
+ * @returns Vertex - itself
+ */
+const findInVertices = (haystack, needle) => {
+    haystack.find( vertex => vertex.id === needle )
+}
+
 
 /**
  * When it has sibs, the seqId shall determine its 
@@ -34,7 +35,7 @@ const vertexChecks = (self) => ({
  * @param any content
  * @returns 
  */
-export default function Vertex (id, seqId, content){
+function Vertex (id, seqId, content){
     let vertex = {
         id,
         seqId,
@@ -42,7 +43,12 @@ export default function Vertex (id, seqId, content){
     }
 
     return Object.assign(
-        vertex,
-        vertexChecks(vertex)
+        vertex
     )
+}
+
+export {
+    Vertex,
+    compareVertices,
+    findInVertices
 }
